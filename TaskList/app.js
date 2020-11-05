a6813d45eed86ce52ec8750a059813eb0e40e07c
TheJackForge <kimfidler@gmail.com>
2020-11-05 14:29:45 -0700

    Coded new array variable and got Completed items working with localStorage

--- TaskList/app.js
+++ TaskList/app.js
@@ -7,6 +7,7 @@ const items = JSON.parse(localStorage.getItem('items')) || [];
 
 
 
+
 button.addEventListener('click', addItems)
 input.addEventListener('keydown', function (e) {
     if (e.key === 'Enter' || e.keyCode === 13) {
@@ -20,41 +21,36 @@ input.addEventListener('keydown', function (e) {
 function addItems() {
     const inputValue = input.value;
     const item = {
-        inputValue: inputValue
+        inputValue: inputValue,
+        complete: false
     }
 
-    // Push inputValue (item) to items array
-    items.push(item);
-    // Push inputValue (item) to local Storage
-    localStorage.setItem('items', JSON.stringify(items));
+
 
     if (inputValue !== '') {
     let li = document.createElement('div');
     
    list.appendChild(li);
     li.innerHTML += `
-    <div class="row mb-1 bg-primary rounded text-light"><div class="col-sm my-auto">${inputValue}</></div><div class="col-sm">
+    <div class="row mb-1 bg-primary rounded text-light checked"><div class="col-sm my-auto checked">${inputValue}</></div><div class="col-sm checked">
 
-    <button class="btn btn-primary btn delete rounded  float-right">Remove</button>
+    <button class="btn btn-primary btn delete rounded float-right checked">Remove</button>
 
-    <button class="btn btn-primary btn edit rounded float-right ">Edit</button>
+    <button class="btn btn-primary btn edit rounded float-right checked">Edit</button>
     
-    <button class="btn btn-primary btn complete rounded  float-right">Complete</button>
+    <button class="btn btn-primary btn complete rounded float-right checked">Complete</button>
 
     </div></div>`
 
-    input.value = '';
+    input.value = '';   
 
-    
-    
-    if (!list.classList.contains('list-populated')) {
-        clear.classList.remove('d-none')
-    }
+
+        // Push inputValue (item) to items array
+        items.push(item);
+        // Push inputValue (item) to local Storage
+        localStorage.setItem('items', JSON.stringify(items));
 }  
 }
-// else {
-//    alert('Please enter an item')
-// }
 
 
     // Remove Button on Created Item - Removes li in app and in localStorage
@@ -70,25 +66,46 @@ function addItems() {
     } 
 })
 
+// Complete Function
+
+// function contains(e) {
+  
+//     if (e.target.classList.contains('complete')) {
+  
+//         const nodes = e.target.parentElement.getElementsByTagName('button');
+//             for (let i=0; i<nodes.length; i++) {
+//             nodes[i].classList.remove('bg-primary', 'text-light')
+//             nodes[i].classList.add('bg-secondary', 'text-muted');
+//         }
+//         const buttonDiv = e.target.parentElement;
+//         const getRow = e.target.parentElement.parentElement;
+//         e.target.classList.remove('bg-primary', 'text-light')
+//         e.target.classList.add('bg-secondary', 'text-muted');
+//         buttonDiv.classList.remove('bg-primary')
+//         getRow.classList.remove('bg-primary', 'text-light');
+//         buttonDiv.classList.add('bg-secondary');
+//         getRow.classList.add('bg-secondary', 'text-muted');
+       
+//     }   
+// }
+
+// (e.target.classList.contains('complete') && 
 
 function contains(e) {
-    if (e.target.classList.contains('complete')) {
-        const nodes = e.target.parentElement.getElementsByTagName('button');
-        for (let i=0; i<nodes.length; i++) {
-        nodes[i].classList.remove('bg-primary', 'text-light')
-        nodes[i].classList.add('bg-secondary', 'text-muted');
-        }
-        const buttonDiv = e.target.parentElement;
-
-        const getRow = e.target.parentElement.parentElement;
-        buttonDiv.classList.remove('bg-primary')
-        getRow.classList.remove('bg-primary', 'text-light');
-
-        buttonDiv.classList.add('bg-secondary');
-        getRow.classList.add('bg-secondary', 'text-muted');
+    for (let i=0; i<items.length; i++)
+    if (e.target.parentElement.previousElementSibling.textContent === items[i].inputValue) {
+     items[i].complete = true;
+     e.target.parentElement.previousElementSibling.classList.add('bg-secondary', 'text-muted')
+     e.target.parentElement.classList.add('bg-secondary', 'text-muted')
+     e.target.parentElement.classList.add('bg-secondary', 'text-muted')
+     e.target.previousElementSibling.classList.add('bg-secondary', 'text-muted')
+     e.target.classList.add('bg-secondary', 'text-muted')
+     e.target.previousElementSibling.previousElementSibling.classList.add('bg-secondary', 'text-muted')
+     e.target.parentElement.parentElement.classList.remove('bg-primary');
+     e.target.parentElement.parentElement.classList.add('bg-secondary', 'text-muted')
+     localStorage.setItem('items', JSON.stringify(items));
+    }
 }
-}
-
 
 // Button to Clear Items from list
 let clearAll = document.querySelector('.clear')
@@ -118,7 +135,7 @@ list.addEventListener('click', function (e) {
 
 window.addEventListener('DOMContentLoaded', function () {
     for (let i=0; i<items.length; i++) {
-
+    if (items[i].complete === false) {
     let li = document.createElement('div');
     list.appendChild(li);
     li.innerHTML += `
@@ -131,7 +148,23 @@ window.addEventListener('DOMContentLoaded', function () {
     <button class="btn btn-primary btn complete rounded  float-right">Complete</button>
 
     </div></div>`
+    } else {
+    let li = document.createElement('div');
+    list.appendChild(li);
+    li.innerHTML += `
+    <div class="row mb-1 bg-secondary rounded text-muted"><div class="col-sm my-auto">${items[i].inputValue}</></div><div class="col-sm">
+
+    <button class="btn btn-secondary btn delete rounded float-right">Remove</button>
+
+    <button class="btn btn-secondary btn edit rounded float-right ">Edit</button>
+    
+    <button class="btn btn-secondary btn complete rounded  float-right">Complete</button>
+
+    </div></div>`
     }
+    }
+
+
     if (!list.classList.contains('list-populated')) {
         clear.classList.remove('d-none')
     }
@@ -139,6 +172,8 @@ window.addEventListener('DOMContentLoaded', function () {
     list.addEventListener('click', contains);
 
     })
+
+
 
    
     
